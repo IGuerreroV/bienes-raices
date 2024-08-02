e85f6a542eb6a15b7c7466e81915b95194ee3fb6
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-02 16:05:04 -0500

    Creando un Header

---
+++ base.php
@@ -0,0 +1,24 @@
+<?php
+    include './includes/templates/header.php';
+?>
+
+    <main class="contenedor seccion">
+        <h1>Titulo Página</h1>
+    </main>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </nav>
+        </div>
+
+        <p class="copyright">Todos los derechos resrvados 2024 &copy;</p>
+    </footer>
+
+    <script src="build/js/bundle.min.js"></script>
+</body>
+</html>
