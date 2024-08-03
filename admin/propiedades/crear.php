35a663713c1ae01a98d683b95d4b63e3f9f4846a
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-03 18:23:02 -0500

    Agregar HTML y formulario para crear propiedades

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,48 @@
         <h1>Crear</h1>
 
         <a class="boton boton-verde" href="/admin">Volver</a>
+
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="1">Ivan</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+
+            <input class="boton boton-verde" type="submit" value="Crear Propiedad">
+        </form>
     </main>
 
 <?php
