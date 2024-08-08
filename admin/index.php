84e19dd22e68639e17cebde7ed179a75a4534875
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-08 10:31:21 -0500

    Mostrar una alerta cuando una propiedad fue creada

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,16 @@
 <?php
+
+    $resultado = $_GET['resultado'] ?? null;
+
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
+        <?php if(intval( $resultado ) === 1): ?>
+            <p class="alerta exito">Anuncio creado correctamente</p>
+        <?php endif; ?>
 
         <a class="boton boton-verde" href="/admin/propiedades/crear.php">Nueva Propiedad</a>
     </main>
