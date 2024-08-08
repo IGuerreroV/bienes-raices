a23383fc7e63aa4f98f9b6b1ee7a282d1074f3bc
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-08 11:29:25 -0500

    Creando una tabla para listar las propiedades de la base de datos

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,31 @@
         <?php endif; ?>
 
         <a class="boton boton-verde" href="/admin/propiedades/crear.php">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img class="imagen-tabla" src="/imagenes/9b06c16bd18b9fd661c9e5820a820324.jpg"></td>
+                    <td>$1200000</td>
+                    <td>
+                        <a class="boton-rojo-block" href="#">Eliminar</a>
+                        <a class="boton-amarillo-block" href="#">Actalizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
